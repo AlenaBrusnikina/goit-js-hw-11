fe19fd52d1683bba12ccbdd2d5e47608fcc5b262
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-28 19:32:38 +0000

    Deploying to gh-pages from @ AlenaBrusnikina/goit-js-hw-11@23efeb72120c80f3072621fba9f26d03c4415165 🚀

---
+++ index.ccd5b5a4.js
@@ -0,0 +1,2 @@
+fetch("".concat("https://pixabay.com/api/","?key=").concat("34824738-bcdb93f3fccf2566ad3e9b4d4","&q=dog&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=").concat(1)).then((function(t){return t.json()})).then((function(t){return console.log(t.hits)})).catch((function(t){return console.log(t)}));
+//# sourceMappingURL=index.ccd5b5a4.js.map
