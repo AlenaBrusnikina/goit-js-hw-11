2af8979b0070b0cd2aa948d5b75425384981fa75
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-29 16:45:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import { fetchImage } from "./fetchImage";
+
+const refs = {
+    searchFormEl: document.querySelector('#search-form'),
+    inputFormEl: document.querySelector('input'),
+    galleryEl: document.querySelector('.gallery'),
+    btnLoadMoreEl: document.querySelector('.load-more'),
+}
 
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '34824738-bcdb93f3fccf2566ad3e9b4d4';
-let page = 1;
-
-fetch(
-    `${BASE_URL}?key=${API_KEY}&q=dog&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-).then((res) => res.json()).then(x => console.log(x.hits)).catch((error) => console.log(error));
